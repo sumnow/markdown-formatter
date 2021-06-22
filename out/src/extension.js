a8f07f605e99c4126b61043a324ab4432c8004cd
mervin <mydiamervin@gmail.com>
2021-06-22 16:46:27 +0800

    remove out/scr file

--- out/src/extension.js
+++ out/src/extension.js
@@ -1,5 +1,6 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = exports.formatted = void 0;
 const vscode = require("vscode");
 const FormatList_1 = require("./components/FormatList");
 const FormatTable_1 = require("./components/FormatTable");
