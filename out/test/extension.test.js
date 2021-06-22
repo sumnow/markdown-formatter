a7474727e7fc37432e669009485de1be8253334f
mervin <mydiamervin@gmail.com>
2021-06-22 16:46:01 +0800

    remove test file

--- out/test/extension.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import { formatted } from '../src/extension'
-// describe('sum suite', function () {
-//     test('should add 2 positive numbers together and return the result', function () {
-//         expect(formatted(`# name`)).toBe(``);
-//     });
-// });
-//# sourceMappingURL=extension.test.js.map
